f957ffd396cc09441d5697d6fd3171810a53bbae
Sheenan Tenepre <sheenan.tenepre@shiftbrain.co.jp>
2016-03-09 00:41:23 +0900

    uodate gradimate.js

--- _src/scripts/libs/gradimate.js
+++ _src/scripts/libs/gradimate.js
@@ -3,24 +3,21 @@ export default class gradimate {
         this.target = $target
         this.config = ( typeof $config !== 'undefined' ) ? $config : this.getDefaults()
 
-        let renderCSS = this.renderCSS()
+        let initCSS = this.initCSS()
+        let applyCSS = this.applyCSS()
     }
 
     getDefaults () {
         return {
             gradientType: 'linear-gradient',
             degrees: '270deg',
-            colorPattern: [
-                '#33f4c2',
-                '#31053c',
-                '#9dcf17'
-            ],
-            backgroundSize: '600% 600%',
-            duration: '10s'
+            colorPattern: ['#33f4c2','#31053c','#9dcf17'],
+            backgroundSize: '600% 600%' ,
+            effect: 'default'
         }
     }
 
-    renderCSS () {
+    initCSS () {
         let css = '@-webkit-keyframes AnimationName{0%,100%{background-position:0 50%}50%{background-position:100% 50%}}@-moz-keyframes AnimationName{0%,100%{background-position:0 50%}50%{background-position:100% 50%}}@-o-keyframes AnimationName{0%,100%{background-position:0 50%}50%{background-position:100% 50%}}@keyframes AnimationName{0%,100%{background-position:0 50%}50%{background-position:100% 50%}}',
         head = document.head || document.getElementsByTagName('head')[0],
         style = document.createElement('style')
@@ -31,5 +28,17 @@ export default class gradimate {
         else style.appendChild( document.createTextNode( css ) )
 
         head.appendChild( style )
+    }
+
+    applyCSS () {
+        let _config = this.config
+        let _target = this.target
+        let _colors = ''
+        _config.colorPattern.map(( item ) => { _colors += `${ item },` })
+        _colors = _colors.substring(0, _colors.length - 1);
+
+        _target.style.background = `${ _config.gradientType }(${ _config.degrees }, ${ _colors })`
+        _target.style.backgroundSize = `${ _config.backgroundSize }`
+        _target.style.animation = 'AnimationName 10s ease infinite'
     }
 }
